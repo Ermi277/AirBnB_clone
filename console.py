be917d5670ccc255062e97d8217b68a5bcc2ca18
Ermiyas <mr.ermiyas@gmail.com>
2022-10-27 12:50:25 +0000

    console.py

--- console.py
+++ console.py
@@ -3,8 +3,9 @@
 
 import cmd
 from models.base_model import BaseModel
+from models import storage
 
-working_classes = {'BaseModel': BaseModel}
+used_classes = {'BaseModel': BaseModel}
 
 class HBNBCommand(cmd.Cmd):
     """The command interpreter Class """
@@ -30,6 +31,10 @@ class HBNBCommand(cmd.Cmd):
         if not validate_classname(args):
             return 
 
+        new_obj = used_classes[args[0]]()
+        new_obj.save()
+        print(new_obj.id)
+
     def do_show(self, arg):
         """ 
             Prints the string representation of an instance based on the 
@@ -41,7 +46,7 @@ class HBNBCommand(cmd.Cmd):
 
         instance_obj = storage.all()
         key = "{}.{}".format(args[0], args[1])
-        chk_instance = instance_obj.get(ke, None)
+        chk_instance = instance_obj.get(key, None)
         if chk_instance is None:
             print("** no instance found **")
             return
@@ -65,9 +70,19 @@ class HBNBCommand(cmd.Cmd):
         """
         
         args = arg.split()
-
+        
         if not validate_classname(args):
             return
+
+        all_objs = storage.all()
+
+        if len(args) < 1:
+            print(["{}".format(str(v)) for _, v in all_objs.items()])
+            return
+
+        print(["{}".format(str(v)) for _, v in 
+            all_objs.items() if type(v).__name__ == args[0]])
+        return
         
 def validate_classname(args, chk_id=False):
     """Check if class_name exist or checks if it is entered"""
@@ -76,7 +91,7 @@ def validate_classname(args, chk_id=False):
         print("** class name missing **")
         return False
 
-    if args[0] not in working_classes.keys():
+    if args[0] not in used_classes.keys():
         print("** class doesn't exist **")
         return False
 
